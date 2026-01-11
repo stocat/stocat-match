6abcb45e49e188a1f7ff0a5e4b3edb649274fe64
seminchoi <choicco89@gmail.com>
2026-01-11 12:40:40 +0900

    init: 프로젝트 생성

---
+++ match-api/src/main/java/com/stocat/matchapi/MatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.stocat.matchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchApiApplication.class, args);
+    }
+
+}
